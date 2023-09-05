d784602401c6fc4e8e83f750259d5a6dc0048a97
Delusoire <71462480+Delusoire@users.noreply.github.com>
2023-09-05 21:21:49 +0100

    Delete dist/full-screen.prism.js

--- dist/full-screen.prism.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-    if (!document.getElementById(`full-screen-js`)) {
-        const el = document.createElement("script")
-        el.id = `full-screen-js`
-        el.textContent = `${await fetch(`https://api.github.com/repos/Delusoire/spicetify-extensions/contents/dist/full-screen.js`).then(res => res.json()).then(data => atob(data.content))}`
-        document.head.appendChild(el)
-    }
-})()
