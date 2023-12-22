7ea377a47a71eb6e7ba4d2ef75157d3ed8ae4f91
Delusoire <71462480+Delusoire@users.noreply.github.com>
2023-12-22 15:07:57 +0100

    y

--- dist/bad-lyrics/app.js
+++ dist/bad-lyrics/app.js
@@ -549,12 +549,12 @@ var AnimatedText = class extends LitElement {
       this.globalRSPSpring.setEquilibrium(index + rsp);
       rsp = this.globalRSPSpring.current - index;
     }
-    if (rsp < 0) {
+    if (rsp === 0) {
       this.style.textShadow = "0 0 var(3.75px,0) rgba(255,255,255,0.5)";
       this.style.backgroundColor = "black";
       this.style.backgroundImage = "unset";
     } else {
-      if (rsp < 1) {
+      if (rsp === 1) {
         this.style.textShadow = "0 0 var(1.25px,0) rgba(255,255,255,0.85)";
       } else {
         const textShadowBlurRadiusPx = rsp * 5;
